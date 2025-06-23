627743ea75528ed5103a7aa2fd2b624b9a2a3046
Dallas Yu <dallas_yu@hotmail.com>
2025-06-23 18:08:50 -0500

    added operators and area of circle method

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+            int sum = a + b;
+            int prod = a * b;
+            int diff = a - b;
+            int quotient = a / b;
+            int remainder = a % b;
+            
+            Console.WriteLine($"{a} + {b} is {sum}");
+            Console.WriteLine($"{a} * {b} is {prod}");
+            Console.WriteLine($"{a} - {b} is {diff}");
+            Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}\n");
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+            Console.WriteLine($"The area of a circle with radius {radius} is {AreaOfCircle(radius)}");
         }
+
+        public static double AreaOfCircle(double radius)
+        {
+            
+            return Math.PI * Math.Pow(radius, 2);
+        }        
     }
 }
